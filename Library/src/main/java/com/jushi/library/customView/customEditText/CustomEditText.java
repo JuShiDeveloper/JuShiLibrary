7af2e1a37bd63b28e9146ddc79f8f6feecef2a6d
JuShi <yufei.wang@longmaster.com.cn>
2019-07-24 18:20:37 +0800

    首次提交

---
+++ Library/src/main/java/com/jushi/library/customView/customEditText/CustomEditText.java
@@ -0,0 +1,356 @@
+package com.jushi.library.customView.customEditText;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.jushi.library.R;
+
+
+public class CustomEditText extends RelativeLayout implements View.OnClickListener, TextWatcher {
+    /**
+     * 输入框
+     */
+    private EditText editText;
+    /**
+     * 清除内容按钮
+     */
+    private ImageView clearBtn;
+    /**
+     * 显示或隐藏密码按钮
+     */
+    private ImageView showPswBtn;
+    /**
+     * 获取验证码的文字按钮
+     */
+    private TextView authCodeBtn;
+
+    private OnAuthCodeButtomClickListener authCodeButtomClickListener;
+    private OnTextChangedListener onTextChangedListener;
+    private int customEditTextType = -1;
+
+    enum PasswordDisplayType {
+        HIDDEN, DISPLAY
+    }
+
+    private PasswordDisplayType passwordDisplayType = PasswordDisplayType.DISPLAY;
+
+    private int passwordHiddenResId = 0; //隐藏密码图片资源id
+    private int passwordDisplayResid = 0; //显示密码图片资源id
+    private int defaultTextSize = 42; //输入文字的默认大小,对应xml中设置的 14sp
+
+    public CustomEditText(Context context) {
+        this(context, null);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(attrs);
+    }
+
+    private void initialize(AttributeSet attrs) {
+        initView();
+        initAttrs(attrs);
+        setListener();
+        setClearButtonVisible(false);
+        setAuthCodeButtonVisible(false);
+        setShowPasswordButtonVisible(false);
+    }
+
+    private void initView() {
+        View.inflate(getContext(), R.layout.custom_edittext_layout, this);
+        editText = findViewById(R.id.et_input_content);
+        clearBtn = findViewById(R.id.iv_clear_btn);
+        showPswBtn = findViewById(R.id.iv_is_show_view);
+        authCodeBtn = findViewById(R.id.tv_auth_code_btn);
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomEditText);
+        setEditTextDigits(typedArray.getString(R.styleable.CustomEditText_digits));
+        setDrawableLeft(typedArray.getResourceId(R.styleable.CustomEditText_drawableLeft, 0));
+        setHintText(typedArray.getString(R.styleable.CustomEditText_hint));
+        setClearButtonDrawable(typedArray.getResourceId(R.styleable.CustomEditText_srcClearButton, 0));
+        passwordHiddenResId = typedArray.getResourceId(R.styleable.CustomEditText_srcPasswordHidden, 0);
+        passwordDisplayResid = typedArray.getResourceId(R.styleable.CustomEditText_srcPasswordDisplay, 0);
+        setShowPasswordButtonDrawable(passwordHiddenResId);
+        setAuthCodeButtonText(typedArray.getString(R.styleable.CustomEditText_authCodeHint));
+        setAuthCodeButtonTextColor(typedArray.getColor(R.styleable.CustomEditText_authCodeTextColor, Color.BLUE));
+        setAuthCodeButtonTextSize(typedArray.getDimensionPixelSize(R.styleable.CustomEditText_authCodeTextSize, defaultTextSize));
+        setInputTextColor(typedArray.getColor(R.styleable.CustomEditText_inputTextColor, Color.BLACK));
+        setHintTextColor(typedArray.getColor(R.styleable.CustomEditText_hintTextColor, Color.LTGRAY));
+        setInputTextSize(typedArray.getDimensionPixelSize(R.styleable.CustomEditText_inputTextSize, defaultTextSize));
+        setMaxLength(typedArray.getInt(R.styleable.CustomEditText_maxLength, 20));
+    }
+
+    private void setListener() {
+        clearBtn.setOnClickListener(this);
+        showPswBtn.setOnClickListener(this);
+        authCodeBtn.setOnClickListener(this);
+        editText.addTextChangedListener(this);
+    }
+
+    /**
+     * 设置清除按钮是否可见
+     *
+     * @param visible true-可见，false-不可见
+     */
+    private void setClearButtonVisible(boolean visible) {
+        clearBtn.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    /**
+     * 设置获取验证码按钮是否可见（默认不可见，在设置了按钮点击事件监听后可见）
+     *
+     * @param visible true-可见，false-不可见
+     */
+    private void setAuthCodeButtonVisible(boolean visible) {
+        authCodeBtn.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * 设置显示/隐藏密码的按钮是否可见(默认不可见)
+     *
+     * @param visible true-可见，false-不可见
+     */
+    public void setShowPasswordButtonVisible(boolean visible) {
+        showPswBtn.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * 设置编辑框输入类型
+     *
+     * @param inputType InputType类中的值 （传入多个使用 | 隔开）
+     */
+    public void setEditTextInputType(int inputType) {
+        editText.setInputType(inputType);
+    }
+
+    /**
+     * 设置输入框可输入的字符
+     *
+     * @param digits
+     */
+    public void setEditTextDigits(String digits) {
+        if (digits == null || digits.equals("")) return;
+        editText.setKeyListener(DigitsKeyListener.getInstance(digits));
+    }
+
+    /**
+     * 设置左边图标
+     *
+     * @param resId
+     */
+    public void setDrawableLeft(int resId) {
+        if (resId == 0) return;
+        Drawable drawable = getContext().getResources().getDrawable(resId);
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+        editText.setCompoundDrawables(drawable, null, null, null);
+    }
+
+    /**
+     * 设置左边图标
+     *
+     * @param drawable
+     */
+    public void setDrawableLeft(Drawable drawable) {
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+        editText.setCompoundDrawables(drawable, null, null, null);
+    }
+
+    /**
+     * 设置输入提示信息
+     *
+     * @param hintText
+     */
+    public void setHintText(String hintText) {
+        if (hintText == null || hintText.equals("")) return;
+        editText.setHint(hintText);
+    }
+
+    public void setClearButtonDrawable(int resId) {
+        if (resId == 0) return;
+        clearBtn.setImageResource(resId);
+    }
+
+    /**
+     * 设置控制密码显示/隐藏按钮图片资源
+     *
+     * @param resId 图片资源id
+     */
+    private void setShowPasswordButtonDrawable(int resId) {
+        if (resId == 0) return;
+        showPswBtn.setImageResource(resId);
+    }
+
+    /**
+     * 设置获取验证码按钮的提示文字
+     *
+     * @param text
+     */
+    public void setAuthCodeButtonText(String text) {
+        if (text == null || text.equals("")) return;
+        authCodeBtn.setText(text);
+    }
+
+    /**
+     * 设置获取验证码按钮文字颜色
+     *
+     * @param color
+     */
+    private void setAuthCodeButtonTextColor(int color) {
+        authCodeBtn.setTextColor(color);
+    }
+
+    /**
+     * 设置获取验证码按钮文字大小
+     *
+     * @param size
+     */
+    private void setAuthCodeButtonTextSize(float size) {
+        authCodeBtn.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
+    }
+
+    /**
+     * 获取输入的内容
+     *
+     * @return 输入的内容
+     */
+    public String getInputText() {
+        return editText.getText().toString();
+    }
+
+    /**
+     * 设置输入文字颜色
+     *
+     * @param color
+     */
+    public void setInputTextColor(int color) {
+        editText.setTextColor(color);
+    }
+
+    /**
+     * 设置提示文字颜色
+     *
+     * @param color
+     */
+    public void setHintTextColor(int color) {
+        editText.setHintTextColor(color);
+    }
+
+    /**
+     * 设置输入文字显示的大小
+     *
+     * @param size
+     */
+    private void setInputTextSize(float size) {
+        editText.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
+    }
+
+    /**
+     * 设置可输入最大字符数
+     *
+     * @param maxLength
+     */
+    private void setMaxLength(int maxLength) {
+        editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.iv_clear_btn) {
+            editText.setText("");
+        } else if (i == R.id.iv_is_show_view) {
+            showPswBtnClickEvent();
+        } else if (i == R.id.tv_auth_code_btn) {
+            if (authCodeButtomClickListener != null)
+                authCodeButtomClickListener.onAuthCodeButtonClick(v);
+        }
+    }
+
+    /**
+     * 处理显示/隐藏密码按钮点击事件，控制密码的显示或隐藏
+     */
+    private void showPswBtnClickEvent() {
+        setShowPasswordButtonDrawable((passwordDisplayType == PasswordDisplayType.HIDDEN) ?
+                passwordHiddenResId : passwordDisplayResid);
+        passwordDisplayType = (passwordDisplayType == PasswordDisplayType.HIDDEN) ?
+                PasswordDisplayType.DISPLAY : PasswordDisplayType.HIDDEN;
+        //根据眼睛图标状态设置输入的密码显示或隐藏
+        editText.setTransformationMethod((passwordDisplayType == PasswordDisplayType.HIDDEN) ?
+                HideReturnsTransformationMethod.getInstance() : PasswordTransformationMethod.getInstance());
+        editText.setSelection(editText.getText().length());//设置光标始终显示在文字最右边
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        setClearButtonVisible(s.length() > 0);
+        if (onTextChangedListener == null || s.length() == 0) return;
+        onTextChangedListener.onTextChanged(s.toString(), customEditTextType);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    /**
+     * 设置获取验证码按钮点击事件监听
+     *
+     * @param authCodeButtomClickListener
+     */
+    public void setAuthCodeButtonClickListener(OnAuthCodeButtomClickListener authCodeButtomClickListener) {
+        this.authCodeButtomClickListener = authCodeButtomClickListener;
+        setAuthCodeButtonVisible(true);
+    }
+
+    /**
+     * 设置文字改变监听
+     *
+     * @param onTextChangedListener
+     * @param type                  type的值为使用者自己定义传入进来，用于区分同一个页面多个输入框的情况
+     */
+    public void setOnTextChangedListener(OnTextChangedListener onTextChangedListener, int type) {
+        this.onTextChangedListener = onTextChangedListener;
+        this.customEditTextType = type;
+    }
+
+    /**
+     * 获取验证码按钮点击接口
+     */
+    public interface OnAuthCodeButtomClickListener {
+        void onAuthCodeButtonClick(View view);
+    }
+
+    /**
+     * 输入框文字改变回调接口
+     */
+    public interface OnTextChangedListener {
+        void onTextChanged(String s, int type);
+    }
+
+}
---
+++ Library/src/main/java/com/jushi/library/utils/FileUtil.java
@@ -0,0 +1,201 @@
+package com.jushi.library.utils;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.jushi.library.base.BaseApplication;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * 文件工具类
+ */
+public class FileUtil {
+    /**
+     * 系统相册的路径
+     *
+     * @return
+     */
+    public static String getSystemPhotoPath() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera";
+    }
+
+    /**
+     * 根据Uri返回文件绝对路径
+     * 兼容了file:///开头的 和 content://开头的情况
+     */
+    public static String getRealFilePathFromUri(final Context context, final Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_FILE.equalsIgnoreCase(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equalsIgnoreCase(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+
+    /**
+     * 检查文件是否存在
+     */
+    public static String checkDirPath(String dirPath) {
+        if (TextUtils.isEmpty(dirPath)) {
+            return "";
+        }
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dirPath;
+    }
+
+    /**
+     * 复制文件
+     *
+     * @param oldPath 文件当前存放的地址
+     * @param newPath 文件复制的目标地址
+     */
+    public static boolean copyFile(String oldPath, String newPath) {
+        boolean isSuccess = false;
+        try {
+            int bytesum = 0;
+            int byteread = 0;
+            File oldfile = new File(oldPath);
+            if (oldfile.exists()) { //文件存在时
+                int size = (int) oldfile.length();
+                InputStream inStream = new FileInputStream(oldPath); //读入原文件
+                FileOutputStream fs = new FileOutputStream(newPath);
+                byte[] buffer = new byte[size];
+                while ((byteread = inStream.read(buffer)) != -1) {
+                    bytesum += byteread; //字节数 文件大小
+                    fs.write(buffer, 0, byteread);
+                }
+                inStream.close();
+                isSuccess = true;
+                notifySystemScanImage(newPath);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
+
+    /**
+     * 通知系统检索刚刚保存的图片
+     */
+    public static void notifySystemScanImage(String imagePath) {
+//        ContentValues values = new ContentValues();
+//        values.put(MediaStore.Images.Media.DATA, imagePath);
+//        values.put(MediaStore.Images.Media.CONTENT_TYPE, "image/jpeg");
+//        Uri uri = BaseApplication.context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//        BaseApplication.context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+        MediaScannerConnection.scanFile(BaseApplication.context,
+                new String[]{new File(imagePath).getAbsolutePath()},
+                new String[]{"image/jpeg"}, new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.v("yufei", "onScanCompleted：path = " + path);
+                        Log.v("yufei", "onScanCompleted：uri = " + uri);
+                    }
+                });
+    }
+
+    /**
+     * 保存图片文件
+     *
+     * @param fileFolderStr 文件地址
+     * @param isDir
+     * @param croppedImage
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public static String saveToFile(String fileFolderStr, boolean isDir, Bitmap croppedImage) throws FileNotFoundException, IOException {
+        File jpgFile;
+        if (isDir) {
+            File fileFolder = new File(fileFolderStr);
+            Date date = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss"); // 格式化时间
+            String filename = format.format(date) + ".jpg";
+            if (!fileFolder.exists()) {
+                fileFolder.mkdirs();
+            }
+            jpgFile = new File(fileFolder, filename);
+        } else {
+            jpgFile = new File(fileFolderStr);
+            if (!jpgFile.getParentFile().exists()) { // 如果目录不存在，则创建一个名为"finger"的目录
+                jpgFile.getParentFile().mkdirs();
+            }
+        }
+        FileOutputStream outputStream = new FileOutputStream(jpgFile); // 文件输出流
+        croppedImage.compress(Bitmap.CompressFormat.JPEG, 70, outputStream);
+        outputStream.close();
+        return jpgFile.getPath();
+    }
+
+    /**
+     * 获取path路径下的图片
+     *
+     * @param path 存有图片的文件夹路径
+     * @return 存有该文件夹路径下图片的集合
+     */
+    public static ArrayList<String> findPicsInDir(String path) {
+        ArrayList<String> photos = new ArrayList<String>();
+        File dir = new File(path);
+        if (dir.exists() && dir.isDirectory()) {
+            for (File file : dir.listFiles(new FileFilter() {
+
+                @Override
+                public boolean accept(File pathname) {
+                    String filePath = pathname.getAbsolutePath();
+                    return (filePath.endsWith(".png") || filePath.endsWith(".jpg") || filePath
+                            .endsWith(".jepg"));
+                }
+            })) {
+                photos.add(file.getAbsolutePath());
+            }
+        }
+//        Collections.sort(photos); //升序排序
+        //降序排序
+        Collections.sort(photos, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        return photos;
+    }
+}
