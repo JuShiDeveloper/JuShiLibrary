94f7fda1e6a37c528b6c8bf616f505274fa36269
JuShi <yufei.wang@longmaster.com.cn>
2019-08-05 17:36:04 +0800

    增加电话号码正则匹配

---
+++ Library/src/main/java/com/jushi/library/utils/PhoneNoUtil.java
@@ -0,0 +1,32 @@
+package com.jushi.library.utils;
+
+import android.text.TextUtils;
+
+/**
+ * 电话号码工具类
+ * create time 2019/8/5
+ *
+ * @author JuShi
+ */
+public class PhoneNoUtil {
+    /**
+     * 判断字符串是否符合手机号码格式
+     * 移动号段: 134,135,136,137,138,139,147,150,151,152,157,158,159,170,178,182,183,184,187,188,198
+     * 联通号段: 130,131,132,145,155,156,170,171,175,176,185,186,166
+     * 电信号段: 133,149,153,170,173,177,180,181,189
+     *
+     * @param phoneNo
+     * @return 待检测的字符串
+     */
+
+    public static boolean isMobileNO(String phoneNo) {
+        // "[1]"代表下一位为数字可以是几，"[0-9]"代表可以为0-9中的一个，"[5,7,9]"表示可以是5,7,9中的任意一位,
+        // [^4]表示除4以外的任何一个,\\d{9}"代表后面是可以是0～9的数字，有9位。
+        String regex = "^((13[0-9])|(14[5,7,9])|(15[^4])|(18[0-9])|(17[0,1,3,5,6,7,8])|(19[8,9])|(166))\\d{8}$";
+        if (TextUtils.isEmpty(phoneNo)) {
+            return false;
+        }
+        return phoneNo.matches(regex);
+    }
+
+}
