b1e554c745545839f1116d2a431a121f495af647
lm <yufei.wang@longmaster.com.cn>
2020-08-06 17:12:31 +0800

    文件上传  带上传进度

--- Library/src/main/java/com/jushi/library/http/UploadFileRequester.java
+++ Library/src/main/java/com/jushi/library/http/UploadFileRequester.java
@@ -1,9 +1,17 @@
 package com.jushi.library.http;
 
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,17 +23,81 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
 
+/**
+ * 文件上传 带上传进度
+ */
 public class UploadFileRequester implements Callback {
+    private final String TAG = UploadFileRequester.class.getSimpleName();
     private static final OkHttpClient httpClient;
+    private OnUploadListener onUploadListener;
 
     static {
         httpClient = new OkHttpClient().newBuilder()
-                .connectTimeout(15, TimeUnit.SECONDS)
-                .readTimeout(15, TimeUnit.SECONDS)
-                .writeTimeout(15, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
                 .build();
     }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (onUploadListener != null)
+                onUploadListener.onProgress(msg.arg1);
+            return false;
+        }
+    });
+
+
+    public void uploadFile(String url, String fileName, OnUploadListener onUploadListener) {
+        this.onUploadListener = onUploadListener;
+        Request request = new Request.Builder()
+                .url(url)
+                .post(getStreamBody(fileName))
+                .build();
+        httpClient.newCall(request).enqueue(this);
+    }
+
+    private RequestBody getStreamBody(final String fileName) {
+        return new RequestBody() {
+
+            @Override
+            public long contentLength() throws IOException {
+                return new File(fileName).length();//若是断点续传则返回剩余的字节数
+            }
+
+            @Override
+            public MediaType contentType() {
+                return MediaType.parse("application/octet-stream");
+//                return MediaType.parse("image/png");
+                //这个根据上传文件的后缀变化，要是不知道用application/octet-stream
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                //方式一：
+                FileInputStream fis = new FileInputStream(new File(fileName));
+                fis.skip(0);//跳到指定位置，断点续传
+                long writeLength = 0;
+                int length;
+                byte[] buffer = new byte[2048];
+                OutputStream outputStream = sink.outputStream();
+                while ((length = fis.read(buffer)) != -1) {
+//                    outputStream.write(buffer, 0, length);
+                    //或者
+                    sink.write(buffer, 0, length);
+                    writeLength += length;
+                    Message message = handler.obtainMessage();
+                    message.arg1 = (int) (writeLength * 1.0f / contentLength() * 100);
+                    handler.sendMessage(message);
+                }
+            }
+        };
+    }
+
 
     /**
      * 上传多个文件
@@ -34,6 +106,7 @@ public class UploadFileRequester implements Callback {
      * @param fileNames
      */
     public void uploadFiles(String url, List<String> fileNames) {
+        Log.v(TAG, "url = " + url);
         Call call = httpClient.newCall(getRequest(url, fileNames));
         call.enqueue(this);
     }
@@ -79,11 +152,20 @@ public class UploadFileRequester implements Callback {
 
     @Override
     public void onFailure(@NotNull Call call, @NotNull IOException e) {
-
+        onUploadListener.onError(e.getMessage());
     }
 
     @Override
     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-
+        onUploadListener.onSucess();
     }
+
+    public interface OnUploadListener {
+        void onError(String msg);
+
+        void onProgress(int progress);
+
+        void onSucess();
+    }
+
 }
--- app/src/main/java/com/library/jushi/jushilibrary/MainActivity.kt
+++ app/src/main/java/com/library/jushi/jushilibrary/MainActivity.kt
@@ -2,12 +2,16 @@ package com.library.jushi.jushilibrary
 
 import android.util.Log
 import com.jushi.library.base.BaseFragmentActivity
+import com.jushi.library.http.UploadFileRequester
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
 
 class MainActivity : BaseFragmentActivity() {
 
-    override fun getLayoutResId(): Int{
-        setSystemBarStatus(true,true)
+    override fun getLayoutResId(): Int {
+        setSystemBarStatus(true, true)
         return R.layout.activity_main
     }
 
@@ -24,10 +28,51 @@ class MainActivity : BaseFragmentActivity() {
 //        Log.v("yufei", DateUtil.getDateLong(Date()).toString())
 //        Log.v("yufei", DateUtil.getDateLong("2012-12-01 12:12:12").toString())
         Log.v("yufei", "activity initData()")
+        //
+        val fileName = "/storage/emulated/0/MagazineUnlock/magazine-unlock-03-2.3.5162-6273E3E4F9C121EBE427369BC2A3434F.jpg"
+        val jsonObject = JSONObject()
+        try {
+            jsonObject.put("op_type", 3015)
+            jsonObject.put("user_id", 1008930)
+            jsonObject.put("task_id", "")
+            jsonObject.put("c_type", 1)
+            jsonObject.put("pid", 5)
+            jsonObject.put("sid", "db576a7d2453575f29eab4bac787b919")
+            jsonObject.put("c_ver", "9010")
+            jsonObject.put("act_type", 1)
+            jsonObject.put("ext", "jpg")
+            jsonObject.put("gender", 1)
+            jsonObject.put("file_size", 160702)
+            jsonObject.put("file_name", "" + System.currentTimeMillis())
+            jsonObject.put("buss_type", 2)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        val url = "http://test.guijk.com/hfs/?json=$jsonObject"
+        val list = ArrayList<String>()
+        list.add(fileName)
+        btn.setOnClickListener {
+            tv_progress.text = "0%"
+            UploadFileRequester().uploadFile(url, fileName, object : UploadFileRequester.OnUploadListener {
+                override fun onError(msg: String?) {
+                    Log.v("yufei", "onError  $msg")
+                }
+
+                override fun onProgress(progress: Int) {
+                    runOnUiThread { tv_progress.text = "$progress%" }
+                    Log.v("yufei", "$progress%")
+                }
+
+                override fun onSucess() {
+                    Log.v("yufei", "onSucess")
+                }
+
+            })
+        }
     }
 
     override fun setListener() {
-        Log.v("yufei", "activity setListener()")
+
     }
 
 }
