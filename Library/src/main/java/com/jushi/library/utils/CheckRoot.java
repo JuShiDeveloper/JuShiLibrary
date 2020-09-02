097c55ac627c6aa834b2981e28ecea56604e3715
lm <yufei.wang@longmaster.com.cn>
2020-09-02 15:34:31 +0800

    新增设备是否有root权限检查工具类

---
+++ Library/src/main/java/com/jushi/library/utils/CheckRoot.java
@@ -0,0 +1,59 @@
+package com.jushi.library.utils;
+
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+/**
+ * 检测设备是否被root
+ * create by wyf on 2020/1/20
+ */
+public class CheckRoot {
+
+    /**
+     * Is rooted boolean.
+     *
+     * @return the boolean
+     */
+    public static boolean isRooted() {
+        // nexus 5x "/su/bin/"
+        String[] paths = {"/system/xbin/", "/system/bin/", "/system/sbin/", "/sbin/", "/vendor/bin/", "/su/bin/"};
+        try {
+            for (int i = 0; i < paths.length; i++) {
+                String path = paths[i] + "su";
+                if (new File(path).exists()) {
+                    String execResult = exec(new String[]{"ls", "-l", path});
+                    Logger.d("CheckRoot", "isRooted=" + execResult);
+                    if (TextUtils.isEmpty(execResult) || execResult.indexOf("root") == execResult.lastIndexOf("root")) {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static String exec(String[] exec) {
+        String ret = "";
+        ProcessBuilder processBuilder = new ProcessBuilder(exec);
+        try {
+            Process process = processBuilder.start();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                ret += line;
+            }
+            process.getInputStream().close();
+            process.destroy();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+}
