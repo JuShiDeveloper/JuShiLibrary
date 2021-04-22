a7062e51638c407aca4a26ff5ec42b2a930cd7be
zqf <1583510917@qq.com>
2021-04-22 14:01:49 +0800

    提交底部导航栏

--- Library/src/main/java/com/jushi/library/customView/bottomNavgationView/BottomNavigationView.java
+++ Library/src/main/java/com/jushi/library/customView/bottomNavgationView/BottomNavigationView.java
@@ -1,13 +1,17 @@
 package com.jushi.library.customView.bottomNavgationView;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.jushi.library.R;
 
@@ -22,25 +26,46 @@ public class BottomNavigationView extends RelativeLayout {
     private List<Fragment> fragmentList;
     private FragmentManager fragmentManager;
     private Fragment curremtFragment;
+    private int tabTextSelectColor;
+    private int tabTextUnSelectColor;
+    private int tabTextSize;
 
     public BottomNavigationView(Context context) {
-        super(context);
-        init();
+        this(context, null);
     }
 
     public BottomNavigationView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
+        this(context, attrs, 0);
     }
 
     public BottomNavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
 
-    private void init() {
+    private void init(AttributeSet attrs) {
         initView();
+        initAttrs(attrs);
         setListener();
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.BottomNavigationView);
+        setTabSelectTextColor(array.getColor(R.styleable.BottomNavigationView_tab_select_text_color, Color.BLACK));
+        setTabUnSelectTextColor(array.getColor(R.styleable.BottomNavigationView_tab_unselect_text_color, Color.GRAY));
+        setTabTextSize(array.getDimensionPixelSize(R.styleable.BottomNavigationView_tab_text_size, 30));
+    }
+
+    private void setTabTextSize(int dimensionPixelSize) {
+        this.tabTextSize = dimensionPixelSize;
+    }
+
+    private void setTabUnSelectTextColor(int color) {
+        this.tabTextUnSelectColor = color;
+    }
+
+    private void setTabSelectTextColor(int color) {
+        this.tabTextSelectColor = color;
     }
 
     private void initView() {
@@ -65,7 +90,9 @@ public class BottomNavigationView extends RelativeLayout {
         if (onTabSelectListener != null)
             onTabSelectListener.onTabSelect(index);
         for (int i = 0; i < tabs.size(); i++) {
-            tabs.get(i).setSelected(index == i);
+            tabs.get(i).getChildAt(0).setSelected(index == i);
+            ((TextView) tabs.get(i).getChildAt(1)).setTextColor(index == i ? tabTextSelectColor : tabTextUnSelectColor);
+            ((TextView) tabs.get(i).getChildAt(1)).setTextSize(TypedValue.COMPLEX_UNIT_PX, tabTextSize);
         }
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         if (curremtFragment == null) {
